479e4c1ebfdaa2c37645d75f0c76706e18555ec2
Danijnog <danielnjunq@gmail.com>
2024-10-11 16:25:29 -0300

    feat: novo embedding (gpt) e sumarização)

--- project/data_visualization/kmeans_plot.py
+++ project/data_visualization/kmeans_plot.py
@@ -2,7 +2,7 @@ import plotly.express as px
 import pandas as pd
 
 def plot_elbow_method(df):
-    fig = px.line(df, x = 'num_clusters', y = 'wcss', title = "Elbow Method", width = 800, height = 600)
+    fig = px.line(df, x = 'num_clusters', y = 'within_cluster_sum_of_squares', title = "Elbow Method", width = 800, height = 600)
 
     return fig
 
